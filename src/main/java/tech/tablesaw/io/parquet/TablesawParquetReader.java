5c88ddbb07e700b4589343f8753bb92411330313
ccleva <christophe.cleva@orange.fr>
2020-11-20 14:37:44 +0100

    Reader/Writer registry: now only with options to avoid issues

--- src/main/java/tech/tablesaw/io/parquet/TablesawParquetReader.java
+++ src/main/java/tech/tablesaw/io/parquet/TablesawParquetReader.java
@@ -6,7 +6,6 @@ import org.apache.hadoop.fs.Path;
 import org.apache.parquet.hadoop.ParquetReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import tech.tablesaw.api.Row;
 import tech.tablesaw.api.Table;
 import tech.tablesaw.io.DataReader;
@@ -14,40 +13,38 @@ import tech.tablesaw.io.ReaderRegistry;
 import tech.tablesaw.io.Source;
 
 public class TablesawParquetReader implements DataReader<TablesawParquetReadOptions> {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(TablesawParquetReader.class);
 
-	private static final TablesawParquetReader INSTANCE = new TablesawParquetReader();
-	
-	static {
-		register(Table.defaultReaderRegistry);
-	}
+  private static final Logger LOG = LoggerFactory.getLogger(TablesawParquetReader.class);
 
-	public static void register(ReaderRegistry registry) {
-		registry.registerExtension("parquet", INSTANCE);
-		registry.registerMimeType("parquet", INSTANCE); // TODO: find parquet mime type
-		registry.registerOptions(TablesawParquetReadOptions.class, INSTANCE);
-	}
+  private static final TablesawParquetReader INSTANCE = new TablesawParquetReader();
 
-	@Override
-	public Table read(final Source source) throws IOException {
-		return read(TablesawParquetReadOptions.builder(source).build());
-	}
+  static {
+    register(Table.defaultReaderRegistry);
+  }
 
-	@Override
-	public Table read(final TablesawParquetReadOptions options) throws IOException {
-		final long start = System.currentTimeMillis();
-		final File file = options.source().file();
-		final Path path = new Path(file.getAbsolutePath());
-		final TablesawReadSupport readSupport = new TablesawReadSupport(options);
-		try (final ParquetReader<Row> reader = ParquetReader.<Row>builder(readSupport, path).build()) {
-			int i = 0;
-			while(reader.read() != null) {
-				i++;
-			}
-			final long end = System.currentTimeMillis();
-			LOG.debug("Finished reading {} rows from {} in {} ms", i, file.getName(), (end - start));
-		}
-		return readSupport.getTable();
-	}
+  public static void register(final ReaderRegistry registry) {
+    registry.registerOptions(TablesawParquetReadOptions.class, INSTANCE);
+  }
+
+  @Override
+  public Table read(final Source source) throws IOException {
+    return read(TablesawParquetReadOptions.builder(source).build());
+  }
+
+  @Override
+  public Table read(final TablesawParquetReadOptions options) throws IOException {
+    final long start = System.currentTimeMillis();
+    final File file = options.source().file();
+    final Path path = new Path(file.getAbsolutePath());
+    final TablesawReadSupport readSupport = new TablesawReadSupport(options);
+    try (final ParquetReader<Row> reader = ParquetReader.<Row>builder(readSupport, path).build()) {
+      int i = 0;
+      while (reader.read() != null) {
+        i++;
+      }
+      final long end = System.currentTimeMillis();
+      LOG.debug("Finished reading {} rows from {} in {} ms", i, file.getName(), (end - start));
+    }
+    return readSupport.getTable();
+  }
 }
