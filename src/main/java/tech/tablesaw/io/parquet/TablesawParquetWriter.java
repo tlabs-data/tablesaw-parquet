2304115e3215627040c47b9d4b91dd150ebe346e
ccleva <christophe.cleva@orange.fr>
2020-11-20 14:37:44 +0100

    Reader/Writer registry: now only with options to avoid issues

--- src/main/java/tech/tablesaw/io/parquet/TablesawParquetWriter.java
+++ src/main/java/tech/tablesaw/io/parquet/TablesawParquetWriter.java
@@ -1,7 +1,6 @@
 package tech.tablesaw.io.parquet;
 
 import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.hadoop.ParquetFileWriter.Mode;
@@ -9,56 +8,71 @@ import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.api.WriteSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import tech.tablesaw.api.Row;
 import tech.tablesaw.api.Table;
 import tech.tablesaw.io.DataWriter;
 import tech.tablesaw.io.Destination;
+import tech.tablesaw.io.WriterRegistry;
 
 public class TablesawParquetWriter implements DataWriter<TablesawParquetWriteOptions> {
 
-	private static final Logger LOG = LoggerFactory.getLogger(TablesawParquetWriter.class);
+  private static final Logger LOG = LoggerFactory.getLogger(TablesawParquetWriter.class);
 
-	@Override
-	public void write(final Table table, final Destination dest) throws IOException {
-		throw new UnsupportedOperationException(
-				"The use of Destination is not yet supported, please use the write(Table, TablesawParquetWriteOptions) method");
-	}
+  private static final TablesawParquetWriter INSTANCE = new TablesawParquetWriter();
 
-	@Override
-	public void write(final Table table, final TablesawParquetWriteOptions options) throws IOException {
-		try(final ParquetWriter<Row> writer = new Builder(new Path(options.destFile.getAbsolutePath()), table)
-				.withWriteMode(Mode.OVERWRITE).build()) {
-			final long start = System.currentTimeMillis();
-			Row row = new Row(table);
-			while(row.hasNext()) {
-				row = row.next();
-				writer.write(row);
-			}
-			final long end = System.currentTimeMillis();
-			LOG.debug("Finished writing {} rows to {} in {} ms", row.getRowNumber() + 1, options.destFile.getName(), (end - start));
-		}
-	}
+  static {
+    register(Table.defaultWriterRegistry);
+  }
 
-	private class Builder extends ParquetWriter.Builder<Row, Builder> {
-		
-		private final Table table;
-		
-		protected Builder(final Path path, final Table table) {
-			super(path);
-			this.table = table;
-		}
+  public static void register(WriterRegistry registry) {
+    registry.registerOptions(TablesawParquetWriteOptions.class, INSTANCE);
+  }
 
-		@Override
-		protected Builder self() {
-			return this;
-		}
+  @Override
+  public void write(final Table table, final Destination dest) throws IOException {
+    throw new UnsupportedOperationException(
+        "The use of Destination is not yet supported, please use the write(Table, TablesawParquetWriteOptions) method");
+  }
 
-		@Override
-		protected WriteSupport<Row> getWriteSupport(final Configuration conf) {
-			return new TablesawWriteSupport(this.table);
-		}
-		
-	}
-	
+  @Override
+  public void write(final Table table, final TablesawParquetWriteOptions options)
+      throws IOException {
+    try (final ParquetWriter<Row> writer =
+        new Builder(new Path(options.destFile.getAbsolutePath()), table)
+            .withWriteMode(Mode.OVERWRITE)
+            .build()) {
+      final long start = System.currentTimeMillis();
+      Row row = new Row(table);
+      while (row.hasNext()) {
+        row = row.next();
+        writer.write(row);
+      }
+      final long end = System.currentTimeMillis();
+      LOG.debug(
+          "Finished writing {} rows to {} in {} ms",
+          row.getRowNumber() + 1,
+          options.destFile.getName(),
+          (end - start));
+    }
+  }
+
+  private class Builder extends ParquetWriter.Builder<Row, Builder> {
+
+    private final Table table;
+
+    protected Builder(final Path path, final Table table) {
+      super(path);
+      this.table = table;
+    }
+
+    @Override
+    protected Builder self() {
+      return this;
+    }
+
+    @Override
+    protected WriteSupport<Row> getWriteSupport(final Configuration conf) {
+      return new TablesawWriteSupport(this.table);
+    }
+  }
 }
