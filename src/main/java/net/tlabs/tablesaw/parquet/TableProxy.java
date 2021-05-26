f7e87fc3f554dd7b3dfa9130f105f3523be8c8b7
ccleva <christophe.cleva@orange.fr>
2021-05-27 00:17:38 +0200

    Initial implementation of TableProxy:
     * Decouple value conversion from value storage
     * Reduce code duplication

---
+++ src/main/java/net/tlabs/tablesaw/parquet/TableProxy.java
@@ -0,0 +1,230 @@
+package net.tlabs.tablesaw.parquet;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.util.List;
+import tech.tablesaw.api.BooleanColumn;
+import tech.tablesaw.api.ColumnType;
+import tech.tablesaw.api.DateColumn;
+import tech.tablesaw.api.DateTimeColumn;
+import tech.tablesaw.api.DoubleColumn;
+import tech.tablesaw.api.FloatColumn;
+import tech.tablesaw.api.InstantColumn;
+import tech.tablesaw.api.IntColumn;
+import tech.tablesaw.api.LongColumn;
+import tech.tablesaw.api.Row;
+import tech.tablesaw.api.ShortColumn;
+import tech.tablesaw.api.StringColumn;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.api.TextColumn;
+import tech.tablesaw.api.TimeColumn;
+import tech.tablesaw.columns.Column;
+import tech.tablesaw.columns.dates.PackedLocalDate;
+import tech.tablesaw.columns.times.PackedLocalTime;
+
+final class TableProxy {
+
+  private final Table table;
+
+  private final boolean[] rowColumnsSet;
+  private final BooleanColumn[] booleanColumns;
+  private final ShortColumn[] shortColumns;
+  private final IntColumn[] intColumns;
+  private final LongColumn[] longColumns;
+  private final FloatColumn[] floatColumns;
+  private final DoubleColumn[] doubleColumns;
+  private final DateColumn[] dateColumns;
+  private final TimeColumn[] timeColumns;
+  private final DateTimeColumn[] dateTimeColumns;
+  private final InstantColumn[] instantColumns;
+  private final StringColumn[] stringColumns;
+  private final TextColumn[] textColumns;
+
+  private Row currentRow = null;
+  private int currentRownum = -1;
+
+  TableProxy(final Table table) {
+    super();
+    this.table = table;
+    final List<Column<?>> columns = table.columns();
+    final int size = columns.size();
+    rowColumnsSet = new boolean[size];
+    booleanColumns = new BooleanColumn[size];
+    shortColumns = new ShortColumn[size];
+    intColumns = new IntColumn[size];
+    longColumns = new LongColumn[size];
+    floatColumns = new FloatColumn[size];
+    doubleColumns = new DoubleColumn[size];
+    dateColumns = new DateColumn[size];
+    timeColumns = new TimeColumn[size];
+    dateTimeColumns = new DateTimeColumn[size];
+    instantColumns = new InstantColumn[size];
+    stringColumns = new StringColumn[size];
+    textColumns = new TextColumn[size];
+    for (int i = 0; i < size; i++) {
+      fillColumnArrays(i, columns.get(i).type());
+    }
+  }
+
+  private void fillColumnArrays(final int colIndex, final ColumnType columnType) {
+    if (columnType == ColumnType.BOOLEAN) {
+      booleanColumns[colIndex] = table.booleanColumn(colIndex);
+    } else if (columnType == ColumnType.SHORT) {
+      shortColumns[colIndex] = table.shortColumn(colIndex);
+    } else if (columnType == ColumnType.INTEGER) {
+      intColumns[colIndex] = table.intColumn(colIndex);
+    } else if (columnType == ColumnType.LONG) {
+      longColumns[colIndex] = table.longColumn(colIndex);
+    } else if (columnType == ColumnType.FLOAT) {
+      floatColumns[colIndex] = table.floatColumn(colIndex);
+    } else if (columnType == ColumnType.DOUBLE) {
+      doubleColumns[colIndex] = table.doubleColumn(colIndex);
+    } else if (columnType == ColumnType.LOCAL_TIME) {
+      timeColumns[colIndex] = table.timeColumn(colIndex);
+    } else if (columnType == ColumnType.LOCAL_DATE) {
+      dateColumns[colIndex] = table.dateColumn(colIndex);
+    } else if (columnType == ColumnType.LOCAL_DATE_TIME) {
+      dateTimeColumns[colIndex] = table.dateTimeColumn(colIndex);
+    } else if (columnType == ColumnType.INSTANT) {
+      instantColumns[colIndex] = table.instantColumn(colIndex);
+    } else if (columnType == ColumnType.STRING) {
+      stringColumns[colIndex] = table.stringColumn(colIndex);
+    } else if (columnType == ColumnType.TEXT) {
+      textColumns[colIndex] = table.textColumn(colIndex);
+    }
+  }
+
+  void appendInstant(final int colIndex, final Instant value) {
+    instantColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendDateTime(final int colIndex, final LocalDateTime value) {
+    dateTimeColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendFloat(final int colIndex, final float value) {
+    floatColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendDouble(final int colIndex, final double value) {
+    doubleColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendTime(final int colIndex, final LocalTime value) {
+    timeColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendString(final int colIndex, final String value) {
+    stringColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendText(final int colIndex, final String value) {
+    textColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendBoolean(final int colIndex, final boolean value) {
+    booleanColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendShort(final int colIndex, final short value) {
+    shortColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendInt(final int colIndex, final int value) {
+    intColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendLong(final int colIndex, final long value) {
+    longColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void appendDate(final int colIndex, final LocalDate value) {
+    dateColumns[colIndex].append(value);
+    rowColumnsSet[colIndex] = true;
+  }
+
+  void startRow() {
+    currentRownum++;
+  }
+
+  void endRow() {
+    for (int i = 0; i < rowColumnsSet.length; i++) {
+      if (!rowColumnsSet[i]) {
+        table.column(i).appendMissing();
+      } else {
+        rowColumnsSet[i] = false;
+      }
+    }
+  }
+
+  Row getCurrentRow() {
+    if (this.currentRow == null) {
+      this.currentRow = table.row(currentRownum);
+    } else {
+      currentRow.at(currentRownum);
+    }
+    return currentRow;
+  }
+
+  boolean getBoolean(final int colIndex, final int rowIndex) {
+    return booleanColumns[colIndex].get(rowIndex);
+  }
+
+  int getShort(final int colIndex, final int rowIndex) {
+    return shortColumns[colIndex].getShort(rowIndex);
+  }
+
+  int getInt(final int colIndex, final int rowIndex) {
+    return intColumns[colIndex].getInt(rowIndex);
+  }
+
+  long getLong(final int colIndex, final int rowIndex) {
+    return longColumns[colIndex].getLong(rowIndex);
+  }
+
+  float getFloat(final int colIndex, final int rowIndex) {
+    return floatColumns[colIndex].getFloat(rowIndex);
+  }
+
+  double getDouble(final int colIndex, final int rowIndex) {
+    return doubleColumns[colIndex].getDouble(rowIndex);
+  }
+
+  String getString(final int colIndex, final int rowIndex) {
+    return stringColumns[colIndex].get(rowIndex);
+  }
+
+  String getText(final int colIndex, final int rowIndex) {
+    return textColumns[colIndex].get(rowIndex);
+  }
+
+  int getDateToEpochDay(final int colIndex, final int rowIndex) {
+    return (int) PackedLocalDate.toEpochDay(dateColumns[colIndex].getIntInternal(rowIndex));
+  }
+
+  long getTimeToNanoOfDay(final int colIndex, final int rowIndex) {
+    return PackedLocalTime.toNanoOfDay(timeColumns[colIndex].getIntInternal(rowIndex));
+  }
+
+  long getInstantToEpochMilli(final int colIndex, final int rowIndex) {
+    return instantColumns[colIndex].get(rowIndex).toEpochMilli();
+  }
+
+  long getDateTimeToEpochMilli(final int colIndex, final int rowIndex) {
+    return dateTimeColumns[colIndex].get(rowIndex).toInstant(ZoneOffset.UTC).toEpochMilli();
+  }
+}
--- src/main/java/net/tlabs/tablesaw/parquet/TablesawWriteSupport.java
+++ src/main/java/net/tlabs/tablesaw/parquet/TablesawWriteSupport.java
@@ -1,6 +1,5 @@
 package net.tlabs.tablesaw.parquet;
 
-import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,24 +14,10 @@ import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName;
 import org.apache.parquet.schema.Type;
 import org.apache.parquet.schema.Types;
-import tech.tablesaw.api.BooleanColumn;
 import tech.tablesaw.api.ColumnType;
-import tech.tablesaw.api.DateColumn;
-import tech.tablesaw.api.DateTimeColumn;
-import tech.tablesaw.api.DoubleColumn;
-import tech.tablesaw.api.FloatColumn;
-import tech.tablesaw.api.InstantColumn;
-import tech.tablesaw.api.IntColumn;
-import tech.tablesaw.api.LongColumn;
 import tech.tablesaw.api.Row;
-import tech.tablesaw.api.ShortColumn;
-import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
-import tech.tablesaw.api.TextColumn;
-import tech.tablesaw.api.TimeColumn;
 import tech.tablesaw.columns.Column;
-import tech.tablesaw.columns.dates.PackedLocalDate;
-import tech.tablesaw.columns.times.PackedLocalTime;
 
 public class TablesawWriteSupport extends WriteSupport<Row> {
 
@@ -40,20 +25,9 @@ public class TablesawWriteSupport extends WriteSupport<Row> {
   private static final Map<ColumnType, PrimitiveTypeName> PRIMITIVE_MAPPING;
   private static final Map<ColumnType, LogicalTypeAnnotation> ANNOTATION_MAPPING;
   private final Table table;
+  private final TableProxy proxy;
   private final MessageType schema;
   private RecordConsumer recordConsumer;
-  private final BooleanColumn[] booleanColumns;
-  private final ShortColumn[] shortColumns;
-  private final IntColumn[] intColumns;
-  private final LongColumn[] longColumns;
-  private final FloatColumn[] floatColumns;
-  private final DoubleColumn[] doubleColumns;
-  private final DateColumn[] dateColumns;
-  private final TimeColumn[] timeColumns;
-  private final DateTimeColumn[] dateTimeColumns;
-  private final InstantColumn[] instantColumns;
-  private final StringColumn[] stringColumns;
-  private final TextColumn[] textColumns;
 
   static {
     PRIMITIVE_MAPPING = new HashMap<>();
@@ -86,50 +60,7 @@ public class TablesawWriteSupport extends WriteSupport<Row> {
     super();
     this.table = table;
     this.schema = internalCreateSchema(table);
-    final int size = table.columnCount();
-    booleanColumns = new BooleanColumn[size];
-    shortColumns = new ShortColumn[size];
-    intColumns = new IntColumn[size];
-    longColumns = new LongColumn[size];
-    floatColumns = new FloatColumn[size];
-    doubleColumns = new DoubleColumn[size];
-    dateColumns = new DateColumn[size];
-    timeColumns = new TimeColumn[size];
-    dateTimeColumns = new DateTimeColumn[size];
-    instantColumns = new InstantColumn[size];
-    stringColumns = new StringColumn[size];
-    textColumns = new TextColumn[size];
-    for (int i = 0; i < size; i++) {
-      fillColumnArrays(i, table.column(i).type());
-    }
-  }
-
-  private void fillColumnArrays(final int i, final ColumnType columnType) {
-    if (columnType == ColumnType.BOOLEAN) {
-      booleanColumns[i] = table.booleanColumn(i);
-    } else if (columnType == ColumnType.SHORT) {
-      shortColumns[i] = table.shortColumn(i);
-    } else if (columnType == ColumnType.INTEGER) {
-      intColumns[i] = table.intColumn(i);
-    } else if (columnType == ColumnType.LONG) {
-      longColumns[i] = table.longColumn(i);
-    } else if (columnType == ColumnType.FLOAT) {
-      floatColumns[i] = table.floatColumn(i);
-    } else if (columnType == ColumnType.DOUBLE) {
-      doubleColumns[i] = table.doubleColumn(i);
-    } else if (columnType == ColumnType.LOCAL_TIME) {
-      timeColumns[i] = table.timeColumn(i);
-    } else if (columnType == ColumnType.LOCAL_DATE) {
-      dateColumns[i] = table.dateColumn(i);
-    } else if (columnType == ColumnType.LOCAL_DATE_TIME) {
-      dateTimeColumns[i] = table.dateTimeColumn(i);
-    } else if (columnType == ColumnType.INSTANT) {
-      instantColumns[i] = table.instantColumn(i);
-    } else if (columnType == ColumnType.STRING) {
-      stringColumns[i] = table.stringColumn(i);
-    } else if (columnType == ColumnType.TEXT) {
-      textColumns[i] = table.textColumn(i);
-    }
+    this.proxy = new TableProxy(table);
   }
 
   public static MessageType createSchema(final Table table) {
@@ -174,32 +105,29 @@ public class TablesawWriteSupport extends WriteSupport<Row> {
         final ColumnType type = column.type();
         recordConsumer.startField(fieldName, i);
         if (type == ColumnType.BOOLEAN) {
-          recordConsumer.addBoolean(booleanColumns[i].get(rowNumber));
+          recordConsumer.addBoolean(proxy.getBoolean(i, rowNumber));
         } else if (type == ColumnType.SHORT) {
-          recordConsumer.addInteger(shortColumns[i].getShort(rowNumber));
+          recordConsumer.addInteger(proxy.getShort(i, rowNumber));
         } else if (type == ColumnType.INTEGER) {
-          recordConsumer.addInteger(intColumns[i].getInt(rowNumber));
+          recordConsumer.addInteger(proxy.getInt(i, rowNumber));
         } else if (type == ColumnType.LONG) {
-          recordConsumer.addLong(longColumns[i].getLong(rowNumber));
+          recordConsumer.addLong(proxy.getLong(i, rowNumber));
         } else if (type == ColumnType.FLOAT) {
-          recordConsumer.addFloat(floatColumns[i].getFloat(rowNumber));
+          recordConsumer.addFloat(proxy.getFloat(i, rowNumber));
         } else if (type == ColumnType.DOUBLE) {
-          recordConsumer.addDouble(doubleColumns[i].getDouble(rowNumber));
+          recordConsumer.addDouble(proxy.getDouble(i, rowNumber));
         } else if (type == ColumnType.STRING) {
-          recordConsumer.addBinary(Binary.fromString(stringColumns[i].get(rowNumber)));
+          recordConsumer.addBinary(Binary.fromString(proxy.getString(i, rowNumber)));
         } else if (type == ColumnType.TEXT) {
-          recordConsumer.addBinary(Binary.fromString(textColumns[i].get(rowNumber)));
+          recordConsumer.addBinary(Binary.fromString(proxy.getText(i, rowNumber)));
         } else if (type == ColumnType.LOCAL_DATE) {
-          recordConsumer.addInteger(
-              (int) PackedLocalDate.toEpochDay(dateColumns[i].getIntInternal(rowNumber)));
+          recordConsumer.addInteger(proxy.getDateToEpochDay(i, rowNumber));
         } else if (type == ColumnType.LOCAL_TIME) {
-          recordConsumer.addLong(
-              PackedLocalTime.toNanoOfDay(timeColumns[i].getIntInternal(rowNumber)));
+          recordConsumer.addLong(proxy.getTimeToNanoOfDay(i, rowNumber));
         } else if (type == ColumnType.LOCAL_DATE_TIME) {
-          recordConsumer.addLong(
-              dateTimeColumns[i].get(rowNumber).toInstant(ZoneOffset.UTC).toEpochMilli());
+          recordConsumer.addLong(proxy.getDateTimeToEpochMilli(i, rowNumber));
         } else if (type == ColumnType.INSTANT) {
-          recordConsumer.addLong(instantColumns[i].get(rowNumber).toEpochMilli());
+          recordConsumer.addLong(proxy.getInstantToEpochMilli(i, rowNumber));
         } else {
           // This should not happen
           throw new UnsupportedOperationException("Unsupported ColumnType: " + type);
