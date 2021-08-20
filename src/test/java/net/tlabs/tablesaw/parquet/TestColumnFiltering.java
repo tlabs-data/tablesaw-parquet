c056b058d4d4f2fd6cd58e47b479de3cbdd9e6c2
ccleva <christophe.cleva@orange.fr>
2021-08-20 19:08:41 +0200

    Implementation and test for column filtering

---
+++ src/test/java/net/tlabs/tablesaw/parquet/TestColumnFiltering.java
@@ -0,0 +1,34 @@
+package net.tlabs.tablesaw.parquet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import tech.tablesaw.api.Table;
+
+class TestColumnFiltering {
+
+    private static final String APACHE_ALL_TYPES_SNAPPY =
+        "target/test/data/parquet-testing-master/data/alltypes_plain.snappy.parquet";
+    
+    @Test
+    void testKeepFirstSix() throws IOException {
+        final Table table = new TablesawParquetReader().read(
+            TablesawParquetReadOptions.builder(APACHE_ALL_TYPES_SNAPPY)
+                .withOnlyTheseColumns("id", "bool_col", "tinyint_col", "smallint_col", "int_col", "bigint_col")
+                .build());
+        assertEquals(6, table.columnCount(), "Wrong number of column");
+    }
+
+    @Test
+    void testKeepLastFive() throws IOException {
+        final Table table = new TablesawParquetReader().read(
+            TablesawParquetReadOptions.builder(APACHE_ALL_TYPES_SNAPPY)
+                .withOnlyTheseColumns("float_col", "double_col", "date_string_col", "string_col", "timestamp_col")
+                .build());
+        assertEquals(5, table.columnCount(), "Wrong number of column");
+    }
+
+}
