541684d2ffa84e8c1341619bb43b5287c7eb86ff
ccleva <christophe.cleva@orange.fr>
2025-06-29 14:49:28 +0200

    Added encrypted files testing

--- src/test/java/net/tlabs/tablesaw/parquet/TestAllParquetTestingFiles.java
+++ src/test/java/net/tlabs/tablesaw/parquet/TestAllParquetTestingFiles.java
@@ -24,29 +24,63 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.stream.Stream;
 
+import org.apache.parquet.crypto.DecryptionKeyRetriever;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import net.tlabs.tablesaw.parquet.TablesawParquetReadOptions.Builder;
 import tech.tablesaw.api.Table;
 
 class TestAllParquetTestingFiles {
 
     private static final String PARQUET_TESTING_FOLDER = "target/test/data/parquet-testing-master/data/";
+    private static final byte[] FOOTER_ENCRYPTION_KEY = new String("0123456789012345").getBytes();
+    private static final byte[] COLUMN_ENCRYPTION_KEY1 = new String("1234567890123450").getBytes();
+    private static final byte[] COLUMN_ENCRYPTION_KEY2 = new String("1234567890123451").getBytes();
+    private static final Map<String, byte[]> COLUMN_KEY_MAP = Map.of(
+        "double_field", COLUMN_ENCRYPTION_KEY1,
+        "float_field", COLUMN_ENCRYPTION_KEY2);
+    private final static StringKeyIdRetriever KEY_RETRIEVER = new StringKeyIdRetriever();
+    
+    static {
+        KEY_RETRIEVER.putKey("kf", FOOTER_ENCRYPTION_KEY);
+        KEY_RETRIEVER.putKey("kc1", COLUMN_ENCRYPTION_KEY1);
+        KEY_RETRIEVER.putKey("kc2", COLUMN_ENCRYPTION_KEY2);
+    }
 
+    // Simple key retriever, based on UTF8 strings as key identifiers
+    static class StringKeyIdRetriever implements DecryptionKeyRetriever{
+
+      private final Hashtable<String,byte[]> keyMap = new Hashtable<String,byte[]>();
+
+      public void putKey(String keyId, byte[] keyBytes) {
+        keyMap.put(keyId, keyBytes);
+      }
+
+      @Override
+      public byte[] getKey(byte[] keyMetaData) {
+        String keyId = new String(keyMetaData, StandardCharsets.UTF_8);
+        return keyMap.get(keyId);
+      }
+    }
+    
     private static Stream<Arguments> listParquetTestingFile() throws IOException {
         return Files.list(Paths.get(PARQUET_TESTING_FOLDER))
             .map(Path::toFile)
-            .filter(TestAllParquetTestingFiles::filterPath)
+            .filter(TestAllParquetTestingFiles::filterUnencryptedParquet)
             .map(Arguments::of);
     }
     
-    private static boolean filterPath(final File file) {
+    private static boolean filterUnencryptedParquet(final File file) {
         if (!file.isFile()) return false;
         final String filename = file.getName();
         // keep only unencrypted parquet files
@@ -68,6 +102,49 @@ class TestAllParquetTestingFiles {
     @MethodSource("listParquetTestingFile")
     void testParquetTestingFile(final File parquetFile) {
         final Table table = new TablesawParquetReader().read(TablesawParquetReadOptions.builder(parquetFile).build());
+        assertNotNull(table);
+    }
+    
+    private static Stream<Arguments> listEncryptedTestingFile() throws IOException {
+        return Files.list(Paths.get(PARQUET_TESTING_FOLDER))
+            .map(Path::toFile)
+            .filter(TestAllParquetTestingFiles::filterEncryptedParquet)
+            .map(Arguments::of);
+    }
+    
+    private static boolean filterEncryptedParquet(final File file) {
+        if (!file.isFile()) return false;
+        final String filename = file.getName();
+        // keep only encrypted parquet files
+        if (!filename.endsWith(".parquet.encrypted")) return false;
+        // skip external key material
+        if(filename.equals("external_key_material_java.parquet.encrypted")) return false;
+        return true;
+    }
+
+    @ParameterizedTest
+    @MethodSource("listEncryptedTestingFile")
+    void testEncryptedTestingFile(final File parquetFile) {
+        final Builder optionBuilder = TablesawParquetReadOptions.builder(parquetFile)
+            .withFooterKey(FOOTER_ENCRYPTION_KEY)
+            .withColumnKeys(COLUMN_KEY_MAP);
+        if(parquetFile.getName().contains("_aad_")) {
+            optionBuilder.withAADPrefix("tester".getBytes());
+        }
+        final Table table = new TablesawParquetReader().read(optionBuilder.build());
+        assertNotNull(table);
+    }
+
+    @ParameterizedTest
+    @MethodSource("listEncryptedTestingFile")
+    void testEncryptedTestingFileWithRetriever(final File parquetFile) {
+        final Builder optionBuilder = TablesawParquetReadOptions
+            .builder(parquetFile)
+            .withKeyRetriever(KEY_RETRIEVER);
+        if(parquetFile.getName().contains("_aad_")) {
+            optionBuilder.withAADPrefix("tester".getBytes());
+        }
+        final Table table = new TablesawParquetReader().read(optionBuilder.build());
         assertNotNull(table);
     }
 }
