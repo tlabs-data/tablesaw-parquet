8e2cfdb90d1413002049ca5210f8ab969a87cf0f
ccleva <christophe.cleva@orange.fr>
2026-05-21 14:07:15 +0200

    Add data/aes256 files testing

--- src/test/java/net/tlabs/tablesaw/parquet/TestAllParquetTestingFiles.java
+++ src/test/java/net/tlabs/tablesaw/parquet/TestAllParquetTestingFiles.java
@@ -50,11 +50,41 @@ class TestAllParquetTestingFiles {
         "float_field", COLUMN_ENCRYPTION_KEY2);
     private static final StringKeyIdRetriever KEY_RETRIEVER = new StringKeyIdRetriever();
     private static final String AAD_KEY = "tester";
+    // AES256 keys
+    private static final byte[] AES256_FOOTER_ENCRYPTION_KEY = new String("01234567890123456789012345678901").getBytes();
+    private static final byte[] AES256_ENCRYPTION_KEY1 = new String("12345678901234567890123456789012").getBytes();
+    private static final byte[] AES256_ENCRYPTION_KEY2 = new String("12345678901234567890123456789013").getBytes();
+    private static final byte[] AES256_ENCRYPTION_KEY3 = new String("12345678901234567890123456789014").getBytes();
+    private static final byte[] AES256_ENCRYPTION_KEY4 = new String("12345678901234567890123456789015").getBytes();
+    private static final byte[] AES256_ENCRYPTION_KEY5 = new String("12345678901234567890123456789016").getBytes();
+    private static final byte[] AES256_ENCRYPTION_KEY6 = new String("12345678901234567890123456789017").getBytes();
+    private static final byte[] AES256_ENCRYPTION_KEY7 = new String("12345678901234567890123456789018").getBytes();
+    private static final byte[] AES256_ENCRYPTION_KEY8 = new String("12345678901234567890123456789019").getBytes();
+    private static final Map<String, byte[]> AES256_COLUMN_KEY_MAP = Map.of(
+        "double_field", AES256_ENCRYPTION_KEY1,
+        "float_field", AES256_ENCRYPTION_KEY2,
+        "boolean_field", AES256_ENCRYPTION_KEY3,
+        "int32_field", AES256_ENCRYPTION_KEY4,
+        "ba_field", AES256_ENCRYPTION_KEY5,
+        "flba_field", AES256_ENCRYPTION_KEY6,
+        "int64_field.list.element", AES256_ENCRYPTION_KEY7,
+        "int96_field", AES256_ENCRYPTION_KEY8);
+    private static final StringKeyIdRetriever AES256_KEY_RETRIEVER = new StringKeyIdRetriever();
     
     static {
         KEY_RETRIEVER.putKey("kf", FOOTER_ENCRYPTION_KEY);
         KEY_RETRIEVER.putKey("kc1", COLUMN_ENCRYPTION_KEY1);
         KEY_RETRIEVER.putKey("kc2", COLUMN_ENCRYPTION_KEY2);
+        // AES256 keys
+        AES256_KEY_RETRIEVER.putKey("kf", AES256_FOOTER_ENCRYPTION_KEY);
+        AES256_KEY_RETRIEVER.putKey("kc1", AES256_ENCRYPTION_KEY1);
+        AES256_KEY_RETRIEVER.putKey("kc2", AES256_ENCRYPTION_KEY2);
+        AES256_KEY_RETRIEVER.putKey("kc3", AES256_ENCRYPTION_KEY3);
+        AES256_KEY_RETRIEVER.putKey("kc4", AES256_ENCRYPTION_KEY4);
+        AES256_KEY_RETRIEVER.putKey("kc5", AES256_ENCRYPTION_KEY5);
+        AES256_KEY_RETRIEVER.putKey("kc6", AES256_ENCRYPTION_KEY6);
+        AES256_KEY_RETRIEVER.putKey("kc7", AES256_ENCRYPTION_KEY7);
+        AES256_KEY_RETRIEVER.putKey("kc8", AES256_ENCRYPTION_KEY8);
     }
 
     private static Stream<Arguments> listParquetTestingFile() throws IOException {
@@ -157,4 +187,47 @@ class TestAllParquetTestingFiles {
         assertEquals(ColumnType.STRING, table.column(columnCount - 1).type());
     }
     
+    private static Stream<Arguments> listAES256TestingFile() throws IOException {
+        return Files.list(Paths.get(PARQUET_TESTING_FOLDER + "aes256/"))
+            .map(Path::toFile)
+            .filter(TestAllParquetTestingFiles::filterAES256Parquet)
+            .map(Arguments::of);
+    }
+    
+    private static boolean filterAES256Parquet(final File file) {
+        if (!file.isFile()) return false;
+        final String filename = file.getName();
+        // keep only encrypted parquet files
+        if (!filename.endsWith(".parquet.encrypted")) return false;
+        return true;
+    }
+
+    @ParameterizedTest
+    @MethodSource("listAES256TestingFile")
+    void testAES256TestingFile(final File parquetFile) {
+        final Builder optionBuilder = TablesawParquetReadOptions
+            .builder(parquetFile)
+            .withFooterKey(AES256_FOOTER_ENCRYPTION_KEY);
+        if(!parquetFile.getName().contains("uniform")) {
+            optionBuilder.withColumnKeys(AES256_COLUMN_KEY_MAP);
+        }
+        if(parquetFile.getName().contains("disable_aad_storage")) {
+            optionBuilder.withAADPrefix(AAD_KEY.getBytes());
+        }
+        final Table table = new TablesawParquetReader().read(optionBuilder.build());
+        assertNotNull(table);
+    }
+
+    @ParameterizedTest
+    @MethodSource("listAES256TestingFile")
+    void testAES256TestingFileWithRetriever(final File parquetFile) {
+        final Builder optionBuilder = TablesawParquetReadOptions
+            .builder(parquetFile)
+            .withKeyRetriever(AES256_KEY_RETRIEVER);
+        if(parquetFile.getName().contains("disable_aad_storage")) {
+            optionBuilder.withAADPrefix(AAD_KEY.getBytes());
+        }
+        final Table table = new TablesawParquetReader().read(optionBuilder.build());
+        assertNotNull(table);
+    }
 }
